419f2fe63043df6bc384fdd3bb6ca1d5dd2f6047
Pawan Paudel <pawanpaudel93@gmail.com>
2023-08-31 17:50:52 +0545

    Arweavekit setup with next

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,174 @@
-import Image from 'next/image'
+"use client";
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+import { Othent } from "arweavekit/auth";
+import { useState } from "react";
+import {
+  createTransaction,
+  signTransaction,
+  postTransaction,
+} from "arweavekit/transaction";
+import { queryAllTransactionsGQL } from "arweavekit/graphql";
+import { Buffer } from "buffer";
+import Transaction from "arweave/node/lib/transaction";
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+async function logIn() {
+  const userDetails = await Othent.logIn({
+    apiId: process.env.NEXT_PUBLIC_OTHENT_API_ID as string,
+  });
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+  console.log("Othent Login details", userDetails);
 }
+
+async function logOut() {
+  await Othent.logOut({
+    apiId: process.env.NEXT_PUBLIC_OTHENT_API_ID as string,
+  });
+
+  console.log("Logged out of Othent");
+}
+
+const toArrayBuffer = (file: Blob) =>
+  new Promise((resolve) => {
+    const fr = new FileReader();
+    fr.readAsArrayBuffer(file);
+    fr.addEventListener("loadend", (evt) => {
+      resolve(evt.target!.result);
+    });
+  });
+
+function Home() {
+  const [files, setFiles] = useState([]);
+
+  function handleFileChange(e: any) {
+    setFiles(e.target.files);
+  }
+
+  async function createArweaveTransaction() {
+    try {
+      console.log(files[0]);
+      const data = (await toArrayBuffer(files[0])) as ArrayBuffer;
+      let creator = "Anon";
+      try {
+        creator = await window.arweaveWallet.getActiveAddress();
+      } catch (e) {}
+      const metaData = [
+        { name: "Creator", value: creator },
+        { name: "Title", value: "Bundlr PNG" },
+        { name: "Content-Type", value: "image/png" },
+      ];
+      const transaction = await createTransaction({
+        data,
+        type: "data",
+        environment: "mainnet",
+        options: {
+          tags: metaData,
+        },
+      });
+      console.log(transaction);
+      const signedTransaction = await signTransaction({
+        environment: "mainnet",
+        createdTransaction: transaction,
+      });
+      console.log("Signed", signedTransaction);
+      const postedTransaction = await postTransaction({
+        environment: "mainnet",
+        transaction: signedTransaction as Transaction,
+      });
+      console.log("Posted", postedTransaction);
+      console.log("Posted", transaction.id);
+      // Example successful id: https://a6gp5qkf2e6mpljgb5ahyk2nobkgtp2zkdqvh2jc6u7j4occw6ha.arweave.net/B4z-wUXRPMetJg9AfCtNcFRpv1lQ4VPpIvU-njhCt44
+    } catch (error: any) {
+      console.log("Transaction failed due to:", error.message);
+    }
+  }
+
+  async function createBundlrTransaction() {
+    try {
+      console.log(files[0]);
+      const data = (await toArrayBuffer(files[0])) as ArrayBuffer;
+      let creator = "Anon";
+      try {
+        creator = await window.arweaveWallet.getActiveAddress();
+      } catch (e) {}
+      const metaData = [
+        { name: "Creator", value: creator },
+        { name: "Title", value: "Bundlr PNG" },
+        { name: "Content-Type", value: "image/png" },
+      ];
+      const transaction = await createTransaction({
+        data: Buffer.from(data),
+        type: "data",
+        environment: "mainnet",
+        options: {
+          tags: metaData,
+          useBundlr: true,
+          signAndPost: true,
+        },
+      });
+      console.log("Posted", transaction.postedTransaction.id);
+      console.log("Posted txn", transaction);
+      // Example successful id: https://a6gp5qkf2e6mpljgb5ahyk2nobkgtp2zkdqvh2jc6u7j4occw6ha.arweave.net/B4z-wUXRPMetJg9AfCtNcFRpv1lQ4VPpIvU-njhCt44
+    } catch (error: any) {
+      console.log("Transaction failed due to:", error.message);
+    }
+  }
+
+  async function queryGQLTxn() {
+    const query = `
+query{
+  transactions(tags: [
+  { name: "Contract-Src", values: ["DG22I8pR_5_7EJGvj5FbZIeEOgfm2o26xwAW5y4Dd14"] }
+  ] first: 100) {
+edges {
+  node {
+    id
+    owner {
+      address
+    }
+    tags {
+      name
+      value
+    }
+    block {
+      timestamp
+    }
+  }
+}
+}
+}
+
+`;
+
+    const res = await queryAllTransactionsGQL(query, {
+      gateway: "arweave.net",
+      filters: {},
+    });
+
+    console.log("This is the result of the query", res);
+  }
+
+  return (
+    <div className="container flex justify-center pt-12">
+      <div className="flex flex-col gap-4 w-1/2">
+        <button className="border-4" onClick={logIn}>
+          LogIn with Othent
+        </button>
+        <button className="border-4" onClick={logOut}>
+          LogOut with Othent
+        </button>
+        <input className="border-4" type="file" onChange={handleFileChange} />
+        <button className="border-4" onClick={createArweaveTransaction}>
+          Create and Post Arweave Transaction
+        </button>
+        <button className="border-4" onClick={createBundlrTransaction}>
+          Create and Post Bundlr Transaction
+        </button>
+        <button className="border-4" onClick={queryGQLTxn}>
+          Query All GQL Transactions
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
